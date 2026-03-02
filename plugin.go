8cb307be306dd55a91a7d13055674b0e0fc91819
tan-zhuo <tanzhuo@aliyun.com>
2026-03-02 21:56:37 +0900

    chore: bump pluginVersion to v1.5.4

--- plugin.go
+++ plugin.go
@@ -16,7 +16,7 @@ import (
 // Plugin metadata
 const (
 	pluginName        = "etcd.distributed.lock"
-	pluginVersion     = "v1.0.0"
+	pluginVersion     = "v1.5.4"
 	pluginDescription = "etcd distributed lock plugin for lynx framework"
 	confPrefix        = "lynx.etcd-lock"
 )
